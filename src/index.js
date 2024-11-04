b33558ab6c3b5bf07577cb27d7864a755dea467a
mikek182 <marcosvinicius@geekwork.com.br>
2024-11-04 17:55:53 -0300

    Chamada do react router dom

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 import './index.css';
 import GerenciadorDeTarefas from './gerenciador-de-tarefas';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 // Cria a raiz e renderiza o componente App
 const root = ReactDOM.createRoot(document.getElementById('root'));

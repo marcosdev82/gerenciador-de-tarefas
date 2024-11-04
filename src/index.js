e20ff2edc8de265ccd2f5404eca686f88ae83a86
mikek182 <marcosvinicius@geekwork.com.br>
2024-11-04 17:49:37 -0300

    Inserindo strict mode

--- src/index.js
+++ src/index.js
@@ -5,7 +5,12 @@ import GerenciadorDeTarefas from './gerenciador-de-tarefas';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<GerenciadorDeTarefas />, document.getElementById('root'));
-
+// Cria a raiz e renderiza o componente App
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <GerenciadorDeTarefas />
+  </React.StrictMode>
+);
  
 reportWebVitals();

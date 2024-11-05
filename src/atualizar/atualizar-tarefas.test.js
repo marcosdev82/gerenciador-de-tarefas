bca8c30c0415f84c61eb4979fdef121919575921
mikek182 <marcosvinicius@geekwork.com.br>
2024-11-05 12:55:02 -0300

    Componente atualizar terefa

--- src/atualizar/atualizar-tarefas.test.js
+++ src/atualizar/atualizar-tarefas.test.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { createRoot } from "react-dom/client";
 import CadastrarTarefas from './cadastar-tarefas';
 
-describe('Teste do componente de cadastrar tarefa', () => {
+describe('Teste do componente de atualizar tarefa', () => {
 
     it('deve rederizar o componente de cadastro de tarefas', () => {
         const div = document.createElement('div');
         const root = createRoot(div); 
-        root.render(<CadastrarTarefas />);
+        root.render(<CadastrarTarefas id={1} />);
     })
 
 })

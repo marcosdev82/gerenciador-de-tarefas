6c0192e356272ce08f6d9852354bc69d4d024ba0
mikek182 <marcosvinicius@geekwork.com.br>
2024-11-05 11:34:34 -0300

    Correção do componente

--- src/listar/listar-tarefas.test.js
+++ src/listar/listar-tarefas.test.js
@@ -1,12 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import ListTarefas from "./listar-tarefas";
 
-describe('Teste do componente de listagem de tarefas', ()  => {
+describe('Teste do componente de listagem de tarefas', () => {
 
-    it('Deve renderizar o componente sem erros', () =>{
-        const div = document.createElement('div')
-        ReactDOM.render(<ListTarefas)
-    })
+    it('Deve renderizar o componente sem erros', () => {
+        const div = document.createElement('div');
+        const root = createRoot(div); 
+        root.render(<ListTarefas />);
+    });
 
-})
+});

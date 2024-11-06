7462fa7f2448b0f25d2060106c4ccd3a470df800
mikek182 <marcosvinicius@geekwork.com.br>
2024-11-06 20:17:53 -0300

    Criando o teste cadastrar tarefa

--- src/cadastrar/cadastar-tarefa.test.js
+++ src/cadastrar/cadastar-tarefa.test.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import { createRoot } from "react-dom/client";
-import CadastrarTarefa from './cadastar-tarefa';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import CadastrarTarefa from './cadastrar-tarefa';
 
 describe('Teste do componente de cadastrar tarefa', () => {
-    it('deve rederizar o componente de cadastro de tarefas', () => {
-        const div = document.createElement('div');
-        const root = createRoot(div); 
-        root.render(<CadastrarTarefa />);
-    })
-})
+    it('deve renderizar o componente de cadastro de tarefas', () => {
+        const { getByText } = render(<CadastrarTarefa />);
+        expect(getByText('Cadastrar')).toBeInTheDocument();
+    });
+
+    it('deve cadastrar uma nova tarefa', () => {
+        const { getByTestId } = render(<CadastrarTarefa />);
+        
+        // Simula a entrada de texto
+        fireEvent.change(getByTestId('txt-tarefa'), { target: { value: 'Testar componente' }});
+        
+        // Simula o clique no botão cadastrar
+        fireEvent.click(getByTestId('btn-cadastrar'));
+        
+        // Verifica se o modal foi exibido com a mensagem de sucesso
+        expect(getByTestId('modal')).toHaveTextContent('Sucesso');
+        expect(getByTestId('modal')).toHaveTextContent('Tarefa adicionada com sucesso!');
+    });
+});

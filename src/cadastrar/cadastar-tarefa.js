d7d9a685083e4408f63d67f60016932a9ee3adb6
mikek182 <marcosvinicius@geekwork.com.br>
2024-11-05 21:56:08 -0300

    Formulário de cadastro

--- src/cadastrar/cadastar-tarefa.js
+++ src/cadastrar/cadastar-tarefa.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Form, Modal, Container, Card } from 'react-bootstrap';
+import { Navigate, Link } from 'react-router-dom';
 
 function CadastrarTarefa() {
-    return <h1>Cadastrar tarefa</h1>
+    return (
+        <Container className="my-5">
+            <h3 className='text-center'>Cadastrar</h3>
+            <Card className="p-5">
+                <Card.Body>
+                    <Form>
+                        <Form.Group>
+                            <Form.Label>Tarefa</Form.Label>
+                            <Form.Control
+                                type='text'
+                                placeholder='Digite a tarefa'
+                                minLength='5'
+                                maxLength='100'
+                                required 
+                            />
+                            <Form.Control.Feedback type="invalid">
+                                A tarefa deve conter ao menos 5 caracteres.
+                            </Form.Control.Feedback>
+                        </Form.Group>
+                    </Form>
+                </Card.Body>
+            </Card> 
+        </Container>
+    );
 }
 
 export default CadastrarTarefa;

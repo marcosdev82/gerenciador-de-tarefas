1a4319960674dbf1a84279017c35296990077bc0
mikek182 <marcosvinicius@geekwork.com.br>
2024-11-07 20:19:20 -0300

    Implentação do userEffect

--- src/listar/listar-tarefas.js
+++ src/listar/listar-tarefas.js
@@ -1,11 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Table } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 function ListTarefas() {
+
+    const [tarefas, setTarefas] = useState([])
+    const [carregarTarefa, setCarregarTarefa] = useState(true)
+
+    useEffect(() => {
+        function obterTarefa() {
+            const tarefasDb = localStorage['tarefas'];
+            let ListTarefas = tarefasDb ? JSON.parse(tarefasDb) : [];
+            setTarefas(ListTarefas);
+        }
+
+        if (carregarTarefa) {
+            obterTarefa();
+            setCarregarTarefa(false);
+        }
+    },[carregarTarefa]);
+
     return (
-        <Link to="/cadastrar" className="btn btn-success btn-small">
-            Nova tarefa
-        </Link>
+        <div className="text-center">
+            <h3>Tarefas a fazer</h3>
+            <Table striped bordered hover responsive data-testid="tabela">
+                <thead>
+                    <tr>
+                        <th>Tarefa</th>
+                        <th>
+                            <Link 
+                                to="/cadastrar" 
+                                className="btn btn-success btn-small m-1"
+                                data-testid="btn-nova-tarefa"
+                            >
+                                <FontAwesomeIcon icon={faPlus}/> Nova tarefa
+                            </Link>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {/* Aqui você pode mapear suas tarefas para exibir na tabela */}
+                    {/* Exemplo: */}
+                    {/* <tr>
+                        <td>Nome da tarefa</td>
+                        <td>Detalhes ou Ações</td>
+                    </tr> */}
+                </tbody>
+            </Table>
+        </div>
     );
 }
 

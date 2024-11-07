b17ba8ff9e2252a96edd49c7f9b444fadb769db9
mikek182 <marcosvinicius@geekwork.com.br>
2024-11-07 08:26:40 -0300

    Refatoraçao do test

--- src/cadastrar/cadastar-tarefa.test.js
+++ src/cadastrar/cadastar-tarefa.test.js
@@ -1,28 +1,29 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
-import { createRoot } from "react-dom/client";
+import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import CadastrarTarefa from './cadastar-tarefa';
+import { MemoryRouter } from 'react-router-dom';
 
 describe('Teste do componente de cadastrar tarefa', () => {
-    
-    it('deve rederizar o componente de cadastro de tarefas', () => {
-        const div = document.createElement('div');
-        const root = createRoot(div); 
-        root.render(<CadastrarTarefa />);
-    })
+    it('deve renderizar o componente de cadastro de tarefas', () => {
+        render(
+            <MemoryRouter>
+                <CadastrarTarefa />
+            </MemoryRouter>
+        );
+    });
 
     it('deve cadastrar uma nova tarefa', () => {
-        const { getByTestId } = render(<CadastrarTarefa />);
-        
-        // Simula a entrada de texto
-        fireEvent.change(getByTestId('txt-tarefa'), { target: { value: 'Testar componente' }});
-        
-        // Simula o clique no botão cadastrar
-        fireEvent.click(getByTestId('btn-cadastrar'));
-        
-        // Verifica se o modal foi exibido com a mensagem de sucesso
-        expect(getByTestId('modal')).toHaveTextContent('Sucesso');
-        expect(getByTestId('modal')).toHaveTextContent('Tarefa adicionada com sucesso!');
+        render(
+            <MemoryRouter>
+                <CadastrarTarefa />
+            </MemoryRouter>
+        );
+
+        fireEvent.change(screen.getByTestId('txt-tarefa'), { target: { value: 'Testar componente' } });
+        fireEvent.click(screen.getByTestId('btn-cadastrar'));
+
+        expect(screen.getByTestId('modal')).toHaveTextContent('sucesso');
+        expect(screen.getByTestId('modal')).toHaveTextContent('Tarefa adicionada com sucesso!');
     });
 });

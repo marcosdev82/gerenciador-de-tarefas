e8c440271a6d4946b28039d8731e479541759a26
mikek182 <marcosvinicius@geekwork.com.br>
2024-11-08 10:34:52 -0300

    Importação dos componentes

--- src/listar/intens-lista-tarefas.js
+++ src/listar/intens-lista-tarefas.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ListTarefas from "./listar-tarefas";
 import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 function ItensListaTarefas(props){
     return ;

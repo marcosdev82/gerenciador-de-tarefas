0511b39cb42deffd95f2bb966af3ba3dd4f5d7b2
mikek182 <marcosvinicius@geekwork.com.br>
2024-11-10 20:42:59 -0300

    Crição da modal

--- src/listar/concluir-tarefa.js
+++ src/listar/concluir-tarefa.js
@@ -19,6 +19,27 @@ function ConcluirTarefa(props) {
                 data-testid="btn-abrir-modal">
                 <FontAwesomeIcon  icon={faClipboardCheck} />
             </Button>
+            <Modal show={exibirModal} onHide={handleFecharModal}
+                data-testid="modal">
+                <Modal.Header closeButton>
+                    <Modal.Title>Concluir Tarefa</Modal.Title>
+                </Modal.Header>
+                <Modal.body>
+                    Deseja realemente concluir a seguinte tarefa?
+                    <br />
+                    <strong>{props.tarefa.nome}</strong>
+                </Modal.body>
+                <Modal.Footer>
+                    <Button variant="primary" onClick={handleConcluirTarefa}
+                        data-testid="concluir">
+                            Sim
+                    </Button>
+                        <Button variant="light" onClick={handleFecharModal}
+                        data-testid="concluir">
+                            Não
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </span>
     );
 

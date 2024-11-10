b16e12fd00f55509448ab4180978f726ba67fa80
mikek182 <marcosvinicius@geekwork.com.br>
2024-11-10 20:22:14 -0300

    Criação do Conluir tarefas e propTypes

---
+++ src/listar/concluir-tarefa.js
@@ -0,0 +1,16 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function ConcluirTarefa(props) {
+
+    return ();
+
+}
+
+ConcluirTarefa.protoTypes = {
+    tarefa: PropTypes.object.isRequired,
+    carregarTarefa: PropTypes.func.isRequired,
+    className: PropTypes.string.isRequired
+} 
+
+export default ConcluirTarefa;

b122aab7f323ac22b3899523d2b83eb05e4e499d
mikek182 <marcosvinicius@geekwork.com.br>
2024-11-11 17:48:50 -0300

    Test concluir tarefa

--- src/listar/concluir-tarefa.test.js
+++ src/listar/concluir-tarefa.test.js
@@ -2,5 +2,28 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import ConcluirTarefa from "./concluir-tarefa";
 import Tarefa from "../models/tarefa.model";
-import { render, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/react';
+
+
+describe('Teste de componente de conclusão de tarefas', () => {
+  
+    const nomeTarefa = 'Tarefa de teste';
+    const tarefa = new Tarefa(new Date().getTime(), nomeTarefa, false);
+  
+    it('Deve renderizar o componente sem erros', () => {
+      // Criação de uma div e renderização com React Testing Library
+      const div = document.createElement('div');
+      const root = createRoot(div);  
+  
+      root.render(
+        <ConcluirTarefa 
+          tarefa={tarefa}
+          recarregarTarefas={() => false}
+        />
+      );
+  
+      // Asserção de que a tarefa está sendo renderizada corretamente
+      expect(screen.getByText(nomeTarefa)).toBeInTheDocument();  
+    });
+  });

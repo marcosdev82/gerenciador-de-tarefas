d8c4d395a4dbffdcab3907880022bf788a876024
mikek182 <marcosvinicius@geekwork.com.br>
2024-11-12 09:08:40 -0300

    Implementação do remover tarefas

---
+++ src/listar/remover-tarefa.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function RemoverTarefa(props) {
+
+}
+
+RemoverTarefa.PropTypes = {
+    tarefa: PropTypes.object.isRequired,
+    recarregarTarefas: PropTypes.func.isRequired
+}
+
+export default RemoverTarefa;

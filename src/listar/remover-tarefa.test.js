588f96eae96a2cbc57ec08824143ffd656c2d091
mikek182 <marcosvinicius@geekwork.com.br>
2024-11-13 11:26:36 -0300

    Importação das dependências

---
+++ src/listar/remover-tarefa.test.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { RemoverTarefa } from './remover-tarefa';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';

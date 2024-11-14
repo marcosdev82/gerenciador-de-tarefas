b536485c61c6975af101a86c7194738bc24ee7e6
mikek182 <marcosvinicius@geekwork.com.br>
2024-11-14 20:57:19 -0300

    Teste de renderização da paginação

--- src/listar/paginacao.test.js
+++ src/listar/paginacao.test.js
@@ -1,5 +1,19 @@
-import React from 'react'
-import { ReactDOM } from 'react-dom'
-import Paginacao from './paginacao'
-import { render, fireEvent } from '@testing-library/react'
-import '@testing-library/jest-dom'
+import React from 'react';
+import ReactDOM from 'react-dom'; // Correção na importação do ReactDOM
+import Paginacao from './paginacao';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+describe('Teste do componente de paginação', () => {
+    it('Deve renderizar o componente sem erros', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(
+            <Paginacao
+                totalItens={10}
+                itemsPorPagina={10}
+                paginaAtual={1}
+                mudarPagina={() => false} />, div
+        );
+        ReactDOM.unmountComponentAtNode(div); // Correção no método de desmontagem
+    });
+});

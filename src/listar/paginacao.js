96127ea50c25c65f2e96422bbf9649d5e24c6312
mikek182 <marcosvinicius@geekwork.com.br>
2024-11-14 16:38:10 -0300

    Gerar primeiro item da paginação

--- src/listar/paginacao.js
+++ src/listar/paginacao.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import { Pagination } from 'react-bootstrap';
-import PropTypes from 'prop-types';
+import { PropTypes } from 'prop-types';
 
 function Paginacao(props) {
 
-    return ()
+    function gerarPrimeiroItem(){
+        return (
+            <Pagination.First
+                key="pagFirt"
+                onClick={() => props.paginaAtual === 1}
+                disabled={props.paginaAtual === 1} />
+        );
+    }
+
+    function obterPaginacao(){
+        let items = [];
+        items.push(gerarPrimeiroItem());
+        return items;
+    }
+
+    return (
+        <Pagination data-testid="paginacao">
+            {obterPaginacao()}
+        </Pagination>
+    )
 
 }
 
-Paginacao.PropTypes = {
+Paginacao.propTypes = {
     totalItens: PropTypes.number.isRequired,
     intensPorPagina: PropTypes.number.isRequired,
     paginaAtual: PropTypes.number.isRequired,

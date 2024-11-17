412a4a813546b83f737d3e6191672f5c25cd0378
mikek182 <marcosvinicius@geekwork.com.br>
2024-11-17 14:37:26 -0300

    Implentação do Prop Types

--- src/listar/ordenacao.js
+++ src/listar/ordenacao.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSort, faSortUp, faSrtDown } from '@fortawesome/free-solid-svg-icons';
+import { faSort, faSortUp, faSortDown } from '@fortawesome/free-solid-svg-icons';
 import PropTypes from 'prop-types';
 
+function Ordenacao(props) {
+
+}
+
+Ordenacao.propTypes = {
+    ordenarAsc: PropTypes.bool.isRequired,
+    ordenarDesc: PropTypes.bool.isRequired
+}
+
+export default Ordenacao;

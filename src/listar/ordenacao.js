c2e408f24e8a635b32c87071c678a7d991c4f1a0
mikek182 <marcosvinicius@geekwork.com.br>
2024-11-17 14:33:58 -0300

    Importando dependencias da ordenação

---
+++ src/listar/ordenacao.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSort, faSortUp, faSrtDown } from '@fortawesome/free-solid-svg-icons';
+import PropTypes from 'prop-types';
+

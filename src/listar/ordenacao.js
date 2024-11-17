7ed5e6739da468241f0a32328673ac90ce41d728
mikek182 <marcosvinicius@geekwork.com.br>
2024-11-17 14:46:18 -0300

    Componente de ordenação

--- src/listar/ordenacao.js
+++ src/listar/ordenacao.js
@@ -5,6 +5,36 @@ import PropTypes from 'prop-types';
 
 function Ordenacao(props) {
 
+    function handleAscDesc(){
+        return (props.ordenarAsc || props.ordenarDesc) ? 'hidden' : '';
+    }
+
+    function handleAsc(){
+        return (props.ordenarAsc ) ? '' : 'hidden';
+    }
+
+    function handleDesc(){
+        return (props.ordenarDesc ) ? 'hidden' : '';
+    }
+
+    return (<span>
+        <FontAwesomeIcon 
+            icon={faSort}
+            className={handleAscDesc}
+            data-testid="faSort"
+        />
+         <FontAwesomeIcon 
+            icon={faSortUp}
+            className={handleAsc}
+            data-testid="faSortUp"
+        />
+        <FontAwesomeIcon 
+            icon={faSortDown}
+            className={handleAsc}
+            data-testid="faSortDown"
+        />
+    </span>)
+
 }
 
 Ordenacao.propTypes = {

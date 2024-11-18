306a6ca9e1e39d6635cc3ee11a87d236e75986d9
mikek182 <marcosvinicius@geekwork.com.br>
2024-11-18 09:22:25 -0300

    Importação das dependencias

--- src/listar/listar-tarefas.test.js
+++ src/listar/listar-tarefas.test.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import ListTarefas from "./listar-tarefas";
+import Tarefa from '../models/tarefa.model';
+import { render, fireEvent} from '@testing-library/react';
+import '@testing-library/jest-dom';
 
 describe('Teste do componente de listagem de tarefas', () => {
     it('Deve renderizar o componente sem erros', () => {
